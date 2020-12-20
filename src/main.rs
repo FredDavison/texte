bc7ebc250e14beec3198bdedd4fe8259883590c1
FredDavison <fred.dev99@gmail.com>
2020-12-20 11:53:48 +0000

    Read using take()

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ use std::io;
 use std::io::Read;
 
 fn main() {
-    let mut buf = [0; 8];
+    let mut buf = [0; 1];
+    let mut i = 0;
 
     loop {
-        let _n = io::stdin().read(&mut buf).ok().expect("error");
-        let char = buf.bytes().next().expect("error").expect("error");
 
-        println!("{:?}", char);
+        let s = io::stdin();
+        let mut handle = s.take(1);  // returns a Read instance
+        let n = handle.read(&mut buf).expect("Error reading");
+
+        let char = buf.bytes().next().expect("").expect("");
+        println!("{}: {:?}, {} bytes", i, char, n);
 
         if char == 113 {
             break;
         };
+        i += 1;
     }
 }
