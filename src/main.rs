5bfed6d0f3a420ff45c6c8147931bbce828a963c
FredDavison <fred.dev99@gmail.com>
2020-12-20 14:05:09 +0000

    Turn off terminal echo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 use std::io::Read;
+use std::os::unix::io::AsRawFd;
+use termios::*;
 
 fn main() {
     let mut buf = [0; 1];
     let mut i = 0;
+    enable_raw_mode();
 
     loop {
-
         let s = io::stdin();
-        let mut handle = s.take(1);  // returns a Read instance
+        let mut handle = s.take(1); // returns a Read instance
         let n = handle.read(&mut buf).expect("Error reading");
 
         let char = buf.bytes().next().expect("").expect("");
@@ -19,4 +21,12 @@ fn main() {
         };
         i += 1;
     }
+}
+
+fn enable_raw_mode() {
+    let fd = io::stdin().as_raw_fd();
+    let mut termios = Termios::from_fd(fd).expect("");
+
+    termios.c_lflag &= !ECHO;
+    tcsetattr(fd, TCSAFLUSH, &termios).expect("Error setting termios constants.");
 }
