caa1367813ce7dbef6e483274b0706b7f3ae7533
FredDavison <fred.dev99@gmail.com>
2020-12-21 20:07:38 +0000

    Use termion Key and TermRead

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+
+fn die(e: std::io::Error) {
+    panic!(e);
+}
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                };
-
-                if c.is_control() {
-                    println!("{:?} {:#b}\r", b, b);
-                } else {
-                    println!("{:?} {:#b} ({})\r", b, b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Ctrl('q') => break,
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} {:#b} ({})\r", c as u8, c as u8, c);
+                    }
                 }
-            }
-            Err(e) => die(e),
+                _ => println!("{:?}\r", key),
+            },
+            Err(err) => die(err),
         }
     }
-}
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!(e);
 }
