192985e611279ae462f9acd5f0baa17bc83178be
FredDavison <fred.dev99@gmail.com>
2020-12-26 14:50:07 +0000

    Refacor into methods

--- src/editor.rs
+++ src/editor.rs
@@ -3,32 +3,42 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
-
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Ctrl('q') => break,
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} {:#b} ({})\r", c as u8, c as u8, c);
-                        }
-                    }
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error)
+            }
+            if self.should_quit {
+                break;
             }
         }
     }
 
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
+    }
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
     }
 }
 
