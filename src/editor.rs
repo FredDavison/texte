b32e6601cbfdab23ee4c9936547b79574b6f92cd
FredDavison <fred.dev99@gmail.com>
2020-12-26 16:37:09 +0000

    Draw rows

--- src/editor.rs
+++ src/editor.rs
@@ -38,9 +38,21 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r")
+        } else{
+            self.draw_rows();
+        print!("{}", termion::cursor::Goto(1, 1));
+        }
         io::stdout().flush()
     }
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
+    }
 }
+
 
 fn read_key() -> Result<Key, std::io::Error> {
     loop {
@@ -51,5 +63,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!(e);
 }
